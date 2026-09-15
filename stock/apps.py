b23075fcb8b4da7ed2a32935716018d3cb9ada65
Salihou <salihoudjires@gmail.com>
2026-09-15 16:14:45 +0000

    Mise a jour du modeles.py avec calcul automatique du total et prix par defaut

--- stock/apps.py
+++ stock/apps.py
@@ -1,5 +1,8 @@
-from django.apps import AppConfig
-
-
-class StockConfig(AppConfig):
-    name = 'stock'
+@admin.register(Vente)
+class VenteAdmin(admin.ModelAdmin):
+    list_display = ('id', 'client', 'vendeur', 'statut', 'total', 'date_vente')
+    list_filter = ('statut', 'date_vente')
+    search_fields = ('client__nom', 'client__telephone', 'id')
+    inlines = [LigneVenteInline]
+    # 'total' est désormais calculé automatiquement
+    readonly_fields = ('total', 'date_vente')
